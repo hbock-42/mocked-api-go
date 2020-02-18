aa480aad31e1c35a9d8ccceac2e1c0c59acb26ec
Hugo Bock <h.bock@easyrecrue.com>
2020-02-18 18:04:03 +0100

    data struct

--- get-post.go
+++ get-post.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 	// "encoding/json"
 )
+
+type Posts struct {
+	Posts []Post `json:"posts"`
+}
 
 type Post struct {
 	UserID int    `json:"userId"`
@@ -14,7 +21,7 @@ type Post struct {
 }
 
 // GetPost returns a comment from id
-func GetPost(id int) {
+func GetPost(id int) (*Post, error) {
 	jsonFile, err := os.Open("./data/posts.json")
 	if err != nil {
 		fmt.Println(err)
@@ -22,4 +29,15 @@ func GetPost(id int) {
 
 	}
 	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var posts Posts
+	json.Unmarshal(byteValue, &posts)
+	// dirty, we assume that the data is ordered
+	if len(posts.Posts) > id-1 {
+		return &posts.Posts[id-1], nil
+	} else {
+		fmt.Println("OUPS")
+		return nil, errors.New("Oups")
+	}
 }
